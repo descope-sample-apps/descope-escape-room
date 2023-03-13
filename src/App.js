6939e600a39bdc1401768cc766094e7467a76540
Gilad Shriki <shriki@descope.com>
2023-03-12 22:38:26 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
+	const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch("https://www.descope-escape-room.com/api/data", {
-			method: "POST",
-			headers: {
-        Accept: "application/json, text/plain, */*",
-				"Content-Type": "application/json",
-		  }})
-      .then(response => response.json())
-      .then(data => setData(data));
-  }, []);
-  console.log(data)
-  return (
-    <div>
-      <code>{data.body}</code>
-        
-    </div>
-  );
+	fetch("https://descope-escape-room-server.preview.descope.org/api/data", {
+		method: "POST",
+		headers: {
+			Accept: "application/json, text/plain, */*",
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => setData(data));
+
+	return (
+		<div>
+			<div id="login">
+			</div>
+			<code>{data.body}</code>
+		</div>
+	);
 }
 
 export default App;
