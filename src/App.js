dac2d5da74a97ed8543ef4701b371af62694f2f3
Gilad Shriki <shriki@descope.com>
2023-03-20 15:46:08 -0700

    fix effect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 	const [data, setData] = useState([]);
 
-	fetch("https://descope-escape-room.com/api/data", {
-		method: "POST",
-		headers: {
-			Accept: "application/json, text/plain, */*",
-			"Content-Type": "application/json",
-		},
-	})
-		.then((response) => response.json())
-		.then((data) => setData(data));
+	useEffect(() => {
+		fetch("https://descope-escape-room.com/api/data", {
+			method: "POST",
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"Content-Type": "application/json",
+			},
+		})
+			.then((response) => response.json())
+			.then((resJson) => {
+				setData(resJson);
+				console.log(resJson);
+				// data = resJson
+			});
+	}, []);
 
 	return (
 		<div>
-			<div id="login">
-			</div>
+			<div id="login"></div>
 			<code>{data.body}</code>
 		</div>
 	);
